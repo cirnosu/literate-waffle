4752ca289253123e56b36401446f8a882faa259b
cirnosu <chocoholic.176@gmail.com>
2017-05-18 11:39:31 -0400

    Add files via upload

---
+++ scrape-it.py
@@ -0,0 +1,62 @@
+# coding: utf8 #i'm pretty sure this did nothing
+import requests, bs4, csv
+
+outputFile = open('output.csv', 'w')
+outputWriter = csv.writer(outputFile)
+
+somestr = '''list of queries each on a new line, no utf8''' #for easy copy-pasting.
+qstr = somestr.replace(' ','+')
+qlst = qstr.split('\n') #if you prefer a different separator, specify it here
+
+urlbase = 'https://www.google.ca/search?q='
+
+for i in qlst:
+
+	phone = '' #reset everything to an empty string so that if it can't find the data it will be a blank 'cell'
+	num = ''
+	webbutt = ''
+	webadd = ''
+	add = ''
+	addsep = ['','','']
+	city = ''
+	prov = ''
+
+	#print urlbase + i 		#debugging
+	res = requests.get(urlbase+i)
+	res.raise_for_status()
+	currpage = bs4.BeautifulSoup(res.text)
+	
+	phone = currpage.select('span[class="_tA"]') #to find the selector for your desired fields, save the object as a file
+													#and search that html for it cause it will be different from a modern browser
+	if phone != []:
+		for j in phone:
+			if "-" in j.getText(): #if it has a dash its a phone number? seemed to work, surprisingly
+				num = j.getText()
+				print num
+		
+	webbutt = currpage.select('div[class="_IGf"] > a[class="fl"]')
+	if webbutt != []:
+		webadd = webbutt[0].get('href')
+		
+	add = currpage.select('span[class="_tA"]')
+	if add != []:
+		for k in add:
+			if "," in k.getText():
+				addsep = k.getText().split(', ')
+				try:
+					city = addsep[1] 
+					#city = addsep[-2]	#city would prob be more accurate from the right, test this
+					
+				except IndexError: 
+					print('bad city format')
+				try:
+					prov = addsep[2][:2]	#example target '22 Gore St E, Perth, ON K7H 1H5'
+				except IndexError:
+					print('bad prov format')
+	try:
+		outputWriter.writerow([i.replace('+', ' '), str(city), str(prov), str(num), str(webadd)]) #if something pulled from google is
+	except UnicodeEncodeError:																	#in a bad format, i don't want an error
+		print('encountered bad unichar')														#message, i much prefer it to move on.
+																								#hence all the error catching.
+	
+outputFile.close()
